2eb66e3bd2acc1b5a84dc9f8e637d2232f0aefdd
swinwin <winter@pixels-and-beans.de>
2012-03-20 16:04:17 +0100

    new stuff

---
+++ swinwin/AllImagesCompleteChecker.js
@@ -0,0 +1,32 @@
+;(function($, window, document){
+
+
+	function AllImagesCompleteChecker(imageSourceArray){
+		
+		var _instance = this;
+		
+		this.allImagesLoadet = 'ALLIMAGESLOADET';
+		
+		init();
+		
+		function init(){
+			
+			//add all extra images, maybe from css ore later requried in browsercache
+			
+			for (var i in imageSourceArray){
+				var image = new Image()
+				image.src = imageSourceArray[i]
+			}
+		}
+		
+		this.check = function() {
+				n = 0;
+				l = document.images.length;
+				for(i=0; i < l; i++) if(document.images[i].complete == true) n++;
+				if(n < l) setTimeout("check()",100); else $(_instance).trigger(_instance.allImagesLoadet);
+		}
+	}
+	
+	window.AllImagesCompleteChecker = AllImagesCompleteChecker;	
+
+}(jQuery, window, document));
