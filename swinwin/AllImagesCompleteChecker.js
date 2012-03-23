b21ecd6e061e344c9074aec3d107961bc7bbae55
swinwin <winter@pixels-and-beans.de>
2012-03-23 10:18:13 +0100

    bug fixed

--- swinwin/AllImagesCompleteChecker.js
+++ swinwin/AllImagesCompleteChecker.js
@@ -11,19 +11,21 @@
 		
 		function init(){
 			
+			
 			//add all extra images, maybe from css ore later requried in browsercache
 			
 			for (var i in imageSourceArray){
 				var image = new Image()
 				image.src = imageSourceArray[i]
 			}
+			console.log('checkinitend')
 		}
 		
 		this.check = function() {
 				n = 0;
 				l = document.images.length;
 				for(i=0; i < l; i++) if(document.images[i].complete == true) n++;
-				if(n < l) setTimeout("check()",100); else $(_instance).trigger(_instance.allImagesLoadet);
+				if(n < l) setTimeout(_instance.check,100); else $(_instance).trigger(_instance.allImagesLoadet);
 		}
 	}
 	
